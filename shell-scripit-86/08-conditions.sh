83b2c5db2fcd9abb08cbc2e8c93d73bfa52f338e
Deepthi <deepthi121416@gmail.com>
2026-03-09 16:30:58 +0530

    shell

--- shell-scripit-86/08-conditions.sh
+++ shell-scripit-86/08-conditions.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-num=10
-if [ $num -lt 10 ]
+# num=10
+# if [ $num -lt 10 ]
+# then
+# echo "number $num is less than 10"
+# elif [ $num -eq 10 ]
+# then
+# echo "number $num is equal to 10"
+# else
+# echo "number $num is greater than 10"
+# fi
+
+#even or odd
+
+echo "enter a number:"
+read num
+echo "number is $num"
+
+if [ $num % 2 eq 0 ]
 then
-echo "number $num is less than 10"
-elif [ $num -eq 10 ]
-then
-echo "number $num is equal to 10"
+echo "number $num is even"
 else
-echo "number $num is greater than 10"
+echo "number $num is odd
 fi
