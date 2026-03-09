0f1e436842b3b913e2ffb02fb1eb6045c61a3c8a
Deepthi <deepthi121416@gmail.com>
2026-03-09 15:20:18 +0530

    shell

--- shell-scripit-86/08-conditions.sh
+++ shell-scripit-86/08-conditions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 num=10
-if[ $num -lt 10 ]
+if [ $num -lt 10 ]
 then
 echo "number $num is less than 10"
 elif [ $num -eq 10 ]
+then
 echo "number $num is equal to 10"
 else
 echo "number $num is greater than 10"
